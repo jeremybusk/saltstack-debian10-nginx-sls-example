bc92865dfce793edc6a4d929cc8e0619571d8502
Jeremy Busk <jeremybusk@gmail.com>
2019-08-26 08:44:22 -0600

    add sudo to salt cmds

--- prep_apply_test.sh
+++ prep_apply_test.sh
@@ -27,23 +27,21 @@ fi
 echo "Install salt master/minion."
 sudo apt-get update || true
 sudo apt-get install -y salt-master salt-minion
-current_user=$(whoami)
-sudo usermod -aG salt "${current_user}" 
 
 sleep 20
 echo "Accept salt key(s)."
-salt-key -A -y
+sudo salt-key -A -y
 
 sleep 20
 echo "Apply nginx sls state as matched in top.sls."
-salt '*' state.apply
+sudo salt '*' state.apply
 
 echo "Run simple tests."
-salt '*' ps.pkill "python3 -u -m http.server 3400" full=true signal=9 || true
+sudo salt '*' ps.pkill "python3 -u -m http.server 3400" full=true signal=9 || true
 
 sudo mkdir -p /srv/www
-salt '*' cmd.run 'echo "hello" > /srv/www/index.html'
-salt '*' cmd.run 'cd /srv/www; python3 -u -m http.server 3400 >>http.log 2>&1 &'
+sudo salt '*' cmd.run 'echo "hello" > /srv/www/index.html'
+sudo salt '*' cmd.run 'cd /srv/www; python3 -u -m http.server 3400 >>http.log 2>&1 &'
 
 ping -c 4 www.example.com
 
